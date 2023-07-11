55580cb041a9d53d2d7df5556fa99741bcef82bb
7eerup <7eerup@gmail.com>
2023-07-11 10:47:47 +0900

    sequelized-edited

--- models/posts.js
+++ models/posts.js
@@ -13,10 +13,31 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
     }
   }
+
   Posts.init({
-    title: DataTypes.STRING,
-    content: DataTypes.STRING,
-    password: DataTypes.STRING
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER
+    },
+    title: {
+      type: DataTypes.STRING
+    },
+    content: {
+      type: DataTypes.STRING
+    },
+    password: {
+      type: DataTypes.STRING
+    },
+    createdAt: {
+      allowNull: false,
+      type: DataTypes.DATE
+    },
+    updatedAt: {
+      allowNull: false,
+      type: DataTypes.DATE
+    }
   }, {
     sequelize,
     modelName: 'Posts',
